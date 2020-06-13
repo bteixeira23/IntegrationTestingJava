9e5f726722af4d83b23af03cf671070c9b274079
bteixeira23 <beatrizleite23@yahoo.com.br>
2020-06-13 19:03:25 -0300

    Criando o scenario GET

--- src/test/java/definition/AgapitoServer.java
+++ src/test/java/definition/AgapitoServer.java
@@ -1,4 +1,37 @@
 package definition;
+/*
+* Created by Beatriz and Jamesson
+* */
 
-public class AgapitoServer {
+import groovy.json.internal.LazyMap;
+
+public class AgapitoServer
+{
+    private static final String URL_USER = "http://agapito-server.herokuapp.com/users/";
+    private static LazyMap fieldsJson = new LazyMap();
+    private static String lastUser;
+
+    public static void addFields(String field, String value) {
+        fieldsJson.put(field, value);
+    }
+
+    public static void clearFields() {
+        fieldsJson.clear();
+    }
+
+    public static LazyMap getFields() {
+        return fieldsJson;
+    }
+
+    public static String getEndPoint() {
+        return URL_USER;
+    }
+
+    public static String getLastUser() {
+        return lastUser;
+    }
+
+    public static void setLastUser(String lastUser) {
+        AgapitoServer.lastUser = lastUser;
+    }
 }
--- src/test/java/steps/MyStepdefs.java
+++ src/test/java/steps/MyStepdefs.java
@@ -1,4 +1,33 @@
-package PACKAGE_NAME;
+package steps;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import definition.AgapitoServer;
+import groovy.json.internal.LazyMap;
+import org.junit.Assert;
+import support.RESTSupport;
 
 public class MyStepdefs {
+    @cucumber.api.java.en.Given("^Cliente quer criar um novo usuario$")
+    public void clienteQuerCriarUmNovoUsuario() {
+        AgapitoServer.clearFields();
+    }
+
+    @And("^Informar o (.*) com valor \"([^\"]*)\"$")
+    public void loginTemQueSer(String field, String value) throws Throwable {
+        AgapitoServer.addFields(field, value);
+    }
+
+    @When("^Quando o cliente clicar em Salvar$")
+    public void quandoOClienteClicarEmSalvar() {
+        RESTSupport.executePost(AgapitoServer.getEndPoint(),AgapitoServer.getFields());
+        AgapitoServer.setLastUser(RESTSupport.key("id").toString());
+        AgapitoServer.clearFields();
+    }
+
+    @And("^usuario teve ver o (.*) com valor \"([^\"]*)\"$")
+    public void usuarioTeveVerOLoginComValor(String key, String value) throws Throwable {
+        Assert.assertEquals(value, RESTSupport.key(key));  }
 }
